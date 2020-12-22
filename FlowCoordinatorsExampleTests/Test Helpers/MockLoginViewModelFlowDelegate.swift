4a304aa415a25754c3e22cdead2e2b9bcfa1436c
Stefan Arambasich <1455368+sarambasich@users.noreply.github.com>
2020-12-22 11:56:45 -0500

    Adds login challenge VC unit test cases

--- FlowCoordinatorsExampleTests/Test Helpers/MockLoginViewModelFlowDelegate.swift
+++ FlowCoordinatorsExampleTests/Test Helpers/MockLoginViewModelFlowDelegate.swift
@@ -1,8 +1,20 @@
 //
-//  File.swift
+//  MockLoginViewModelFlowDelegate.swift
 //  FlowCoordinatorsExampleTests
 //
 //  Created by Stefan Arambasich on 12/22/20.
 //
 
 import Foundation
+@testable import FlowCoordinatorsExample
+
+
+final class MockLoginViewModelFlowDelegate: LoginViewModelFlowDelegate {
+
+    private(set) var didLogInWasCalled = false
+
+    func didLogIn() {
+        didLogInWasCalled = true
+    }
+
+}
